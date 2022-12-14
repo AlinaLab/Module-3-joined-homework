537141cfdc4c51a3756e3afa4a7506a17ddd71f4
AlinaLab <labunska.alina@gmail.com>
2022-12-14 19:55:55 +0200

    def pay_all

--- system.py
+++ system.py
@@ -125,17 +125,21 @@ class Company:
             ) % (employee, employee.hourly_rate, employee.amount)
             logger.info(msg)
 
-    def pay_all(self) -> None:
-        """Pay all the employees in this company"""
-
+    def pay_all(self, employees: list) -> None:
         for employee in employees:
             self.pay(employee)
-            return f"Pay {employee.fullname} {self.pay(employee)}"
 
         # TODO: implement this method
 
 
-employee_1 = Employee(first_name="Ihor", last_name="Kozakov", role="CEO")
-employee_2 = Employee(first_name="Georg", last_name="Kirichenko", role="manager")
-employee_3 = Employee(first_name="Olha", last_name="Okpenko", role="dev")
-employees = [employee_1, employee_2, employee_3]
+def main():
+    employee_1 = HourlyEmployee(first_name="Ihor", last_name="Kozakov", role="CEO", amount=20)
+    employee_2 = SalariedEmployee(first_name="Georg", last_name="Kirichenko", role="manager")
+    employee_3 = HourlyEmployee(first_name="Olha", last_name="Okpenko", role="dev", amount=60)
+    employees = [employee_1, employee_2, employee_3]
+    company = Company(title='google', employees=employees)
+    company.pay_all(employees)
+
+
+if __name__ == "__main__":
+    main()
