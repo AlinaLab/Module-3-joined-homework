47e0159b9a7a879826861020067871614c81e665
AlinaLab <labunska.alina@gmail.com>
2022-12-14 20:33:50 +0200

    def pay_all

--- system.py
+++ system.py
@@ -1,6 +1,5 @@
 """
 A very advanced employee management system
-
 """
 
 import logging
@@ -23,7 +22,7 @@ class Employee:
 
     @property
     def fullname(self):
-        return self.first_name, self.last_name
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self) -> str:
         """Return a string version of an instance"""
@@ -33,24 +32,29 @@ class Employee:
     def take_holiday(self, payout: bool = False) -> None:
         """Take a single holiday or a payout vacation"""
 
-        remaining = self.vacation_days
         if payout:
-            if self.vacation_days < 5:
-                msg = f"{self} have not enough vacation days. " \
-                      f"Remaining days: %d. Requested: %d" % (remaining, 5)
-                raise ValueError(msg)
-            self.vacation_days -= 5
-            msg = "Taking a holiday. Remaining vacation days: %d" % remaining
-            logger.info(msg)
+            try:
+                if self.vacation_days < 5:
+                    msg = f"{self} have not enough vacation days. " \
+                          f"Remaining days: %d. Requested: %d" % (self.vacation_days, 5)
+                    raise ValueError(msg)
+                self.vacation_days -= 5
+                msg = f"{self} taking a holiday. Remaining vacation days: %d" % self.vacation_days
+                logger.info(msg)
+            except ValueError as er:
+                logger.info(er)
         else:
-            if self.vacation_days < 1:
-                remaining = self.vacation_days
-                msg = f"{self} have not enough vacation days. " \
-                      f"Remaining days: %d. Requested: %d" % (remaining, 1)
-                raise ValueError(msg)
-            self.vacation_days -= 1
-            msg = "Taking a payout. Remaining vacation days: %d" % remaining
-            logger.info(msg)
+            try:
+                if self.vacation_days < 1:
+                    remaining = self.vacation_days
+                    msg = f"{self} have not enough vacation days. " \
+                          f"Remaining days: %d. Requested: %d" % (self.vacation_days, 1)
+                    raise ValueError(msg)
+                self.vacation_days -= 1
+                msg = f"{self} taking a payout. Remaining vacation days: %d" % self.vacation_days
+                logger.info(msg)
+            except ValueError as er:
+                logger.info(er)
 
 
 # noinspection PyTypeChecker
@@ -79,8 +83,21 @@ class SalariedEmployee(Employee):
 class Company:
     """A company representation"""
 
-    title: str
-    employees: list[Employee] = []
+    def __init__(self, title: str, employees: list = []):
+        self.title = title
+        self.employees = employees
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'Company (title = {self.title}, employees = {self.employees})'
+
+    def add_to_company(self, employee: Employee):
+        if isinstance(employee, Employee):
+            self.employees.append(employee)
+        if isinstance(employee, list):
+            self.employees += employee
 
     def get_ceos(self) -> list[Employee]:
         """Return employees list with role of CEO"""
@@ -88,7 +105,7 @@ class Company:
         result = []
         for employee in self.employees:
             if employee.role == "CEO":
-                result.append(employee)
+                result.append(employee.fullname)
         return result
 
     def get_managers(self) -> list[Employee]:
@@ -97,7 +114,7 @@ class Company:
         result = []
         for employee in self.employees:
             if employee.role == "manager":
-                result.append(employee)
+                result.append(employee.fullname)
         return result
 
     def get_developers(self) -> list[Employee]:
@@ -106,7 +123,7 @@ class Company:
         result = []
         for employee in self.employees:
             if employee.role == "dev":
-                result.append(employee)
+                result.append(employee.fullname)
         return result
 
     @staticmethod
@@ -117,17 +134,17 @@ class Company:
             msg = (
                 "Paying monthly salary of %.2f to %s"
             ) % (employee.salary, employee)
-            logger.info(f"Paying monthly salary to {employee}")
+            logger.info(msg)
 
         if isinstance(employee, HourlyEmployee):
             msg = (
-                "Paying %s hourly rate of %.2f for %d hours"
-            ) % (employee, employee.hourly_rate, employee.amount)
+                "Paying %s hourly rate of %.2f for %d hours. Total: %.2f"
+            ) % (employee, employee.hourly_rate, employee.amount, employee.hourly_rate*employee.amount)
             logger.info(msg)
 
     def pay_all(self, employees: list) -> None:
         """Pay all the employees in this company"""
-        
+
         for employee in employees:
             self.pay(employee)
 
@@ -145,3 +162,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
